833cd30fcfce42725e7ca82f72b717a9cdf939f6
harishbabub121000ce <35776855+harishbabub121000ce@users.noreply.github.com>
2018-08-24 22:43:13 +0530

    Express Js added

---
+++ index.js
@@ -0,0 +1,16 @@
+/** Loading the Require Module **/
+
+var express = require('express');
+
+var app  = express();
+
+/*app.get("/",function(req, res) {
+	//console.log("for testing");
+	res.sendFile('/index.html');
+});*/
+
+/*app.listen(8080, function(err, res) {
+	if(!err) {
+		console.log("Server Started Successfully on 8080");
+	}
+});*/
